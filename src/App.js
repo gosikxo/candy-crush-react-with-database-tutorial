957a77f670d464ff55a638be2f98ac8a6c1e86d2
Gosia Brozek <gosikbrozek1997@gmail.com>
2023-03-22 16:05:34 +0100

    Create board with random colors and add it to the state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,33 @@
-function App () {
+import { useEffect, useState } from "react"
+
+const width = 8
+const candyColors = [
+  'blue',
+  'green',
+  'orange',
+  'purple',
+  'yellow',
+  'red'
+]
+
+const App = () => {
+  const [currentColorArrangement, setCurrentColorArrangement] = useState([])
+
+  const createBoard = () => {
+    const randomColorArrangement = []
+    for (let i = 0; i < width * width; i++) {
+      const randomColor = candyColors[Math.floor(Math.random() * candyColors.length)]
+      randomColorArrangement.push(randomColor)
+    }
+    setCurrentColorArrangement(randomColorArrangement)
+  }
+
+  useEffect(() => {
+    createBoard()
+  }, [])
+
   return (
-    <div className="App">
+    <div className="app">
 
     </div>
   )
