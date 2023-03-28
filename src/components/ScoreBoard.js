6e800636c8defda824d04b58ca0d61fc66eac863
Gosia Brozek <gosikbrozek1997@gmail.com>
2023-03-28 15:29:28 +0200

    Fetch data and add it to the state

--- src/components/ScoreBoard.js
+++ src/components/ScoreBoard.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
-export const ScoreBoard = ({score}) => {
+export const ScoreBoard = ({ score }) => {
+  const [data, setData] = useState(null)
+
+  const fetchData = async () => {
+    const response = await axios.get('http://localhost:8000/scores')
+    setData(response.data.data)
+  }
+
+  console.log(data)
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
   return (
     <div className='score-board'>
       <h2>Score: {score}</h2>
